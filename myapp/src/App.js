d5978bc62214fcad7cc4eaa43cc589cccc6e9805
Mohit <mohit.sahu.edunext@gmail.com>
2023-06-20 17:23:39 +0530

    work for dark mode

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,28 +1,37 @@
-import './App.css';
-import TextForm from './component/TextForm';
-import Navbar from './component/Navbar';
-import Alert from './component/Alert';
-import React, {useState} from 'react'
+import "./App.css";
+import TextForm from "./component/TextForm";
+import Navbar from "./component/Navbar";
+import Alert from "./component/Alert";
+import React, { useState } from "react";
 
 function App() {
+  const [alert, setAlert] = useState(null);
+  const [mode,setMode] = useState('light');
 
-const [alert, setAlert] = useState(null);
-
-const showAlert = (message,type)=>{
-  setAlert({
-    msg:message,
-    type: type
-  })
-}
+  const toggleMode = () => {
+    if(mode === 'light'){
+      setMode('dark');
+    }else{
+      setMode('light');
+    }
+  }
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    });
+    setTimeout(() => {
+      setAlert(null)
+    }, 1500);
+  };
 
   return (
     <>
-    <Navbar title="TextUtils" about="about TextUtils"/>
-    <Alert alert = {alert}/>
-    <div className='container my-3'>
-      <TextForm heading = "Enter the text here" showAlert = {showAlert}/>
-    </div>
-    
+      <Navbar title="TextUtils" about="about TextUtils" mode = {mode} toggleMode = {toggleMode}/>
+      <Alert alert={alert} />
+      <div className="container my-3">
+        <TextForm heading="Enter the text here" showAlert={showAlert} />
+      </div>
     </>
   );
 }
--- myapp/src/component/TextForm.js
+++ myapp/src/component/TextForm.js
@@ -1,65 +1,109 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
 
 export default function TextForm(props) {
+  const [text, setText] = useState("");
 
-  const [text,setText] = useState('');
-
-  const handlUpperCase = ()=>{
+  const handlUpperCase = () => {
     setText(text.toUpperCase());
-    props.showAlert("Text converted in UpperCase","success");
-  }
+    props.showAlert("Text converted in UpperCase", "success");
+  };
 
-  const handleLowerCase = ()=>{
+  const handleLowerCase = () => {
     setText(text.toLowerCase());
-    props.showAlert("Text converted in LowerCase","success");
-  }
+    props.showAlert("Text converted in LowerCase", "success");
+  };
 
-  const handleClearData = ()=>{
-    setText('');
-    props.showAlert("Text Cleard","success");
-  }
+  const handleClearData = () => {
+    if(text.length>0){
+      setText("");
+    props.showAlert("Text Cleard", "success");
+    }else{
+      props.showAlert("Text not available", "warning");
+    }
+    
+  };
 
-  const handleCopyData = ()=>{
-    const copytext = document.getElementById('textarea');
+  const handleCopyData = () => {
+    const copytext = document.getElementById("textarea");
     copytext.select();
     navigator.clipboard.writeText(copytext.value);
-    props.showAlert("Text Copied","success");
-  }
-
-
-const handleExtraSpace = ()=>{
-  var string = "";
-  var ar = text.split(" ");
-  ar.forEach((element)=>{
-    if((element !== "")){
-      string = string+" "+element;
+    if(copytext.value.length>0){
+      props.showAlert("Text Copied", "success");
+    }else{
+      props.showAlert("Text not available", "warning");
     }
+    
+  };
+
+  const handleExtraSpace = () => {
+    var string = "";
+    var ar = text.split(" ");
+    ar.forEach((element) => {
+      if (element !== "") {
+        string = string + " " + element;
+      }
     });
     setText(string.trim());
-    props.showAlert("Extra Space Removed","success");
-}
+    props.showAlert("Extra Space Removed", "success");
+  };
 
-
-  const handleonChange = (event)=>{
+  const handleonChange = (event) => {
     setText(event.target.value);
-  }
-  
-  
+  };
+
   return (
     <>
-    <h2>{props.heading}</h2>
-    <div className="form-floating">
-        <textarea className="form-control" id="textarea" onChange = {handleonChange} value={text} style={{height: '200px'}}></textarea>
-        <button className = 'btn btn-primary my-2' onClick={handlUpperCase}  type='Button'>Convert to Upper Case</button>
-        <button className = 'btn btn-primary my-2 mx-2' onClick={handleLowerCase} type='Button'>Convert to Lower Case</button>
-        <button className = 'btn btn-primary my-2 mx-2' onClick={handleClearData} type='Button'>Clear Data</button>
-        <button className = 'btn btn-primary my-2 mx-2' onClick={handleCopyData} type='Button'>Copy Data</button>
-        <button className = 'btn btn-primary my-2 mx-2' onClick={handleExtraSpace} type='Button'>Remove Extra Space</button>
+      <h2>{props.heading}</h2>
+      <div className="form-floating">
+        <textarea
+          className="form-control"
+          id="textarea"
+          onChange={handleonChange}
+          value={text}
+          style={{ height: "200px" }}
+        ></textarea>
+        <button
+          className="btn btn-primary my-2"
+          onClick={handlUpperCase}
+          type="Button"
+        >
+          Convert to Upper Case
+        </button>
+        <button
+          className="btn btn-primary my-2 mx-2"
+          onClick={handleLowerCase}
+          type="Button"
+        >
+          Convert to Lower Case
+        </button>
+        <button
+          className="btn btn-primary my-2 mx-2"
+          onClick={handleClearData}
+          type="Button"
+        >
+          Clear Data
+        </button>
+        <button
+          className="btn btn-primary my-2 mx-2"
+          onClick={handleCopyData}
+          type="Button"
+        >
+          Copy Data
+        </button>
+        <button
+          className="btn btn-primary my-2 mx-2"
+          onClick={handleExtraSpace}
+          type="Button"
+        >
+          Remove Extra Space
+        </button>
         <h1>Your text summary</h1>
-        <p>{text.length > 0 ? text.split(" ").length :0} Word and {text.length}</p>
+        <p>
+          {text.length > 0 ? text.split(" ").length : 0} Word and {text.length}
+        </p>
         <h1>Preview</h1>
         <p>{text}</p>
-    </div>
+      </div>
     </>
-  )
+  );
 }
