33db25e0647519700a13fb2501b5e51537117b30
Mohit <mohit.sahu.edunext@gmail.com>
2023-06-22 14:33:21 +0530

    testing complete

--- myapp/src/component/TextForm.js
+++ myapp/src/component/TextForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 
 export default function TextForm(props) {
-  const [text, setText] = useState(" pleace enter here");
+  const [text, setText] = useState("");
 
   const handlUpperCase = () => {
     setText(text.toUpperCase());
